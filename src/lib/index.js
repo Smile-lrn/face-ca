405ee6d2cebc8d4e85503e3a224625dc58dc9097
lrn <lrn@zjca.com.cn>
2020-07-08 16:08:35 +0800

    删除不必要的axios

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,20 +1,9 @@
-import './axiosConfig'
 import Face from './face.vue' // 导入组件
 var face = {};
 face.install = function (Vue, options) {
     Vue.component('Face', Face)
     if (typeof window !== 'undefined' && window.Vue) {
         window.Vue.use(Face);
-    }
-    // 截图传给后端接口的错误回调函数
-    Vue.prototype.axiosError = function (data) {
-        // 逻辑...
-        console.log('请求出错处理函数')
-    }
-    // 截图传给后端接口的成功回调函数
-    Vue.prototype.axiosSuccess = function (err) {
-        // 逻辑...
-        console.log('请求成功处理函数')
     }
 }
 export default face
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'lib-flexible/flexible.js'
-import axios from 'axios';
 import face from './lib/index.js'
-Vue.prototype.$axios = axios;
 
 Vue.use(face)
 new Vue({
